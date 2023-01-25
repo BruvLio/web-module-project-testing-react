a4ec26a1c383098f51f5878b7f224108021af0c2
Lionel Mbayu <Lionel.Mbayu@gmail.com>
2023-01-25 10:55:49 -0600

    Completed Assignment

--- src/components/Episode.js
+++ src/components/Episode.js
@@ -5,7 +5,8 @@ const Episode = (props) => {
   const { id, image, name, season, number, summary, runtime } = episode;
   const imgsrc = image || 'https://i.ibb.co/2FsfXqM/stranger-things.png';
 
-  console.log("episode: ", episode);
+  // console.log(image);
+  // console.log("episode: ", episode);
 
   return (
     <div className="episode" key={id}>
